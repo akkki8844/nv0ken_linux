ba98bffd92c29b163e42ecc2a174f6de64e70a84
akkki8844 <mulchandaniakshay39@gmail.com>
2026-07-05 03:35:03 +0530

    Select NASM over make builtin assembler

--- toolchain.mk
+++ toolchain.mk
@@ -1,13 +1,12 @@
-CC      ?= gcc
-LD      ?= ld
-AS      ?= nasm
-AR      ?= ar
-OBJCOPY ?= objcopy
+CC      := gcc
+LD      := ld
+AS      := nasm
+AR      := ar
+OBJCOPY := objcopy
 
-HOSTCC  ?= gcc
+HOSTCC  := gcc
 
 COMMON_WARNINGS := -Wall -Wextra -Wpedantic
 FREESTANDING_CFLAGS := -std=c11 $(COMMON_WARNINGS) \
     -ffreestanding -fno-stack-protector -fno-pic -fno-pie \
     -fno-builtin -m64 -mno-red-zone -O2 -g
-
